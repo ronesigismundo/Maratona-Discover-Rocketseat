a9a2aa06ab15ab5a06c4a8dd0537c8416ac8a290
Rone <rone_sigismundo@hotmail.com>
2021-02-19 23:38:35 -0300

    Corrigindo sistema de contas e calculos

--- scripts.js
+++ scripts.js
@@ -2,14 +2,15 @@ const Modal = {
   activeModalTransaction() {
     // Abrir ou fecha modal
     // Adicionar ou remove a class active ao modal
-    Accounts.updateSelect();
+    DOM.createTransactionForm('income');
+    //Accounts.updateSelect();    
     document.querySelector(".modal-overlay.transaction").classList.toggle("active");
   },
 
   activeModalAccount() {
     // Abrir ou fecha modal
     // Adicionar ou remove a class active ao modal
-    Accounts.updateSelect();
+    //Accounts.updateSelect();
     document.querySelector(".modal-overlay.account").classList.toggle("active");
   },
 
@@ -42,10 +43,6 @@ const Storage = {
   }
 
 }
-// Eu preciso somar as entradas
-// depois eu preciso somar as saídas e
-// remover das entradas o valor das saídas
-// assim, eu terei o total
 
 const Transaction = {
   all: Storage.getTransactions(),
@@ -65,7 +62,7 @@ const Transaction = {
   incomes() {
     let income = 0;
     Transaction.all.map(transaction => {
-      if(transaction.amount > 0)
+      if(transaction.type === 'income')
       income += transaction.amount
     })
     return income;
@@ -74,7 +71,7 @@ const Transaction = {
   expenses() {
     let expense = 0;
     Transaction.all.map(transaction => {
-      if(transaction.amount < 0)
+      if(transaction.type === 'expense')
       expense += transaction.amount
     })
 
@@ -82,7 +79,7 @@ const Transaction = {
   },
 
   total() {
-    return this.incomes() + this.expenses()
+    return this.incomes() - this.expenses()
   },
 };
 
@@ -133,29 +130,53 @@ const Accounts = {
   },
 
   updateSelect() {
-    const select = document.querySelector('select#accounts')
+    //const select = document.querySelector('select#accounts')
     let options = "";
 
     Accounts.all.map(account => {
       options += `<option value="${account.id}">${account.institution}</option>`
     })
 
-    select.innerHTML = options    
+    return options    
   },
 
   calc() {
     Accounts.clearBalance();
     Transaction.all.map( transaction => {
-      const accountId = Number(transaction.account)
+      const {type: transactionType, accountIncome, accountExpense, amount} = transaction
 
-      Accounts.all.map((account, key) => {
-        let {id, type, institution, balance} = account
-        if(id === accountId) {
-          balance += Number(transaction.amount);
-          Accounts.all[key] = {id, type, institution, balance: balance}
-        }
-      })
+      if (transactionType === 'income') {
+        this.update(transactionType, Number(accountIncome), amount )
+
+      } else if (transactionType === 'expense'){
+        this.update(transactionType, Number(accountExpense), amount )
+      } else if (transactionType === 'transfer') {
+        this.update("expense", Number(accountExpense), amount )
+        this.update("income", Number(accountIncome), amount )
+      }
+      
     })
+  },
+
+  update(transactionType, accountId, amount) {
+    Accounts.all.map((account, key) => {
+      let {id, type, institution, balance} = account
+
+      if(id === accountId) {
+        if(transactionType === "income"){
+          balance += Number(amount)
+        } else if(transactionType === "expense") {
+          balance -= Number(amount)
+        }
+        Accounts.all[key] = {id, type, institution, balance: balance}
+      }
+    })
+  },
+
+  allfind(account) {
+    const {institution} = this.all.find( element => Number(element.id) === Number(account))    
+
+    return institution;
   },
 
   clearBalance() {
@@ -168,10 +189,10 @@ const Historic = {
   create(accountId) {
     let historic = [];      
     Transaction.all.map(transaction => {     
-      const {description, account, amount, date} = transaction
+      const {type, description, accountIncome, accountExpense, amount, date} = transaction
       
-      if (Number(account) === Number(accountId) ){
-        historic.push({date: date, description: description, amount: amount})
+      if ((Number(accountIncome) || Number(accountExpense)) === Number(accountId) ){
+        historic.push({date, type, description, amount})
       }
     })
 
@@ -188,6 +209,7 @@ const DOM = {
   accountContainer: document.querySelector('#accounts-table tbody'),
   historicContainer: document.querySelector('#historic-table tbody'),
   historicTitle: document.getElementById('historicTitle'),
+  FormTransactionContainer: document.querySelector('form .form-body'),
 
   addTransaction(transaction, index) {
     const tr = document.createElement('tr')
@@ -198,17 +220,37 @@ const DOM = {
   },
 
   innerHTMLTransaction(transaction, index) {
-    const cssClass = transaction.amount > 0 ? "income" : "expense";
-    
-    const {institution} = Accounts.all.find( account => Number(account.id) === Number(transaction.account))
+    const { type, accountIncome, accountExpense, description, amount, date} = transaction;
+    let typeTransaction = "";
+    let cssClass = "";
+    let institution = "";
 
-    const amount = Utils.formatCurrency(transaction.amount)
+    if (type === "income") {
+      typeTransaction = "Entrada";
+      cssClass = "income"
+      institution = Accounts.allfind(accountIncome);
+    } else if ( type === "expense") {
+      typeTransaction = "Saída";
+      cssClass = "expense"
+      institution = Accounts.allfind(accountExpense);
+    } else if ( type === "transfer") {
+      typeTransaction = "Transferência";
+      cssClass = "transfer"
+      institution = `${Accounts.allfind(accountExpense)} >> ${Accounts.allfind(accountIncome)}`;
+    }
+
+    /*const cssClass = transaction.amount > 0 ? "income" : "expense";*/
+    
+    //const {institution} = Accounts.all.find( account => Number(account.id) === Number(transaction.account))
+
+    const value = Utils.formatCurrency(amount)
     
     const html = `        
-            <td class="description">${transaction.description}</td>
+            <td class="type">${typeTransaction}</td>
+            <td class="description">${description}</td>
             <td class="account">${institution}</td>
-            <td class="${cssClass}">${amount}</td>
-            <td class="date">${transaction.date}</td>
+            <td class="${cssClass}">${value}</td>
+            <td class="date">${date}</td>
             <td>
               <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação" />
             </td>    
@@ -249,12 +291,26 @@ const DOM = {
   },
 
   innerHTMLHistoric(historic) {
-    const cssClass = historic.amount > 0 ? "income" : "expense";
+    let cssClass = "";
+    let typeTransaction = "";
+
+    if (historic.type === "income"){
+      typeTransaction = "Entrada";
+      cssClass = "income"
+    } else if (historic.type === "expense"){
+      typeTransaction = "Saída";
+      cssClass = "expense"
+    } else {
+      typeTransaction = "Transferência";
+      cssClass = "transfer"
+    }
+
 
     const amount = Utils.formatCurrency(historic.amount)
     
     const html = `
             <td class="date">${historic.date}</td>        
+            <td class="typeTransaction">${typeTransaction}</td>        
             <td class="description">${historic.description}</td>
             <td class="${cssClass}">${amount}</td>    
     `;
@@ -265,6 +321,77 @@ const DOM = {
   innerHTMLHistoricTitle(accountId) {
     const { institution } = Accounts.all.find(account => Number(account.id) === Number(accountId))
     DOM.historicTitle.innerHTML = institution
+  },
+
+  createTransactionForm(type) {
+    let select = Accounts.updateSelect();    
+    let accountExpense = `<option value="">Conta</option>`;     
+    let accountIncome = `<option value="">Conta</option>`;
+
+    if (type === "transfer") {
+          accountExpense = `<option value="">Conta de origem</option>`;
+          accountIncome = `<option value="">Conta de destino</option>`;      
+    }
+
+    const typeTransaction = `<input type="text" id="typeTransaction" name="typeTransaction" value="${type}"  hidden/>`
+    
+    let account = `<div class="input-group">
+                      <label class="sr-only" for="accounts">Conta</label>
+                      <select id="account-expense" name="account-expense" value="0" >${accountExpense} ${select}</select>
+                    </div>
+                    <div class="input-group">
+                    <label class="sr-only" for="accounts">Conta</label>
+                    <select id="account-income" name="account-income" value="0" >${accountIncome} ${select}</select>
+                  </div>`
+                
+    const description = `<div class="input-group">
+                          <label class="sr-only" for="description">Descrição</label>
+                          <input
+                          type="text"
+                          id="description"
+                          name="description"
+                          placeholder="Descrição"
+                        />`
+    const amount = `<div class="input-group">
+                      <label class="sr-only" for="amount">Valor</label>
+                      <input
+                        type="number"
+                        step="0.01"
+                        id="amount"
+                        name="amount"
+                        placeholder="0,00"
+                      />
+                      <small class="help">
+                       Use , (virgula) para casas decimais
+                      </small>                           
+                    </div>`                    
+    const date = `<div class="input-group">
+                    <label class="sr-only" for="date">Data</label>
+                    <input type="date" id="date" name="date" placeholder="date" />
+                  </div>`
+
+    DOM.innerHTMLForm({ type,
+                        typeTransaction,
+                        account,
+                        description,
+                        amount,
+                        date
+    })              
+
+  },
+
+  innerHTMLForm(form){
+    const {type, typeTransaction, account, description, amount, date} = form;    
+    DOM.clearTabsForm();
+    document.getElementById(`tab-${type}`).classList.add('active');
+
+    DOM.FormTransactionContainer.innerHTML = typeTransaction + account + description + amount + date;
+
+    let accountIncome = document.querySelector('#account-income');
+    let accountExpense = document.querySelector('#account-expense');
+
+    type === "income" || type === "transfer" ? accountIncome.hidden = false : accountIncome.hidden = true
+    type === "expense" || type === "transfer" ? accountExpense.hidden = false : accountExpense.hidden = true
   },
 
   updateBalance() {
@@ -294,6 +421,13 @@ const DOM = {
   clearHistoric() {
     DOM.historicContainer.innerHTML = "";
     DOM.historicTitle.innerHTML = "";
+  },
+
+  clearTabsForm() {
+    document.getElementById('tab-income').classList.remove('active');
+    document.getElementById('tab-expense').classList.remove('active');
+    document.getElementById('tab-transfer').classList.remove('active');
+
   }
 
 };
@@ -324,56 +458,78 @@ const Utils = {
   }
 }
 
-const FormTransaction = {
-  description: document.querySelector('input#description'),
-  account: document.querySelector('select#accounts'),
-  amount: document.querySelector('input#amount'),
-  date: document.querySelector('input#date'),
+const FormTransaction = {  
 
-  getValues() {
+  getElements() {
+    const type= document.querySelector('#typeTransaction');
+    const accountIncome= document.querySelector('select#account-income');
+    const accountExpense= document.querySelector('select#account-expense');
+    const description= document.querySelector('input#description');
+    const amount= document.querySelector('input#amount');
+    const date= document.querySelector('input#date');
+
     return {
-      description: this.description.value,
-      account: this.account.value,
-      amount: this.amount.value,
-      date: this.date.value,
+      type,
+      accountIncome,
+      accountExpense,
+      description,
+      amount,
+      date,
     }
   },
 
+  getValues() {
+    const {type, accountIncome, accountExpense, description, amount, date} = this.getElements();
+    return {
+      type: type.value,
+      accountIncome: accountIncome.value,
+      accountExpense: accountExpense.value,
+      description: description.value,
+      amount: amount.value,
+      date: date.value,
+    }
+  },
+
+  validadeField() {
+    const {type, description, accountIncome, accountExpense, amount, date} = this.getValues()
+    
+    if(type.trim() === "") throw new Error("Por favor, escolhar um tipo de transação")
+    if(type.trim() === "income" && accountIncome.trim() === "") throw new Error("Por favor, escolhar um tipo de conta")
+    if(type.trim() === "expense" && accountExpense.trim() === "") throw new Error("Por favor, escolhar um tipo de conta")
+    if(description.trim() === "") throw new Error("Preencha o campo de descrição")
+    if(amount.trim() === "") throw new Error("Preencha o valor")
+    if(date.trim() === "") throw new Error("Informe a data da transação")
+  },
+
   formatValues() {
-    let {description, account, amount, date} = this.getValues()
+    let {type, description, accountIncome, accountExpense, amount, date} = this.getValues()
 
     amount = Utils.formatAmount(amount)
 
     date = Utils.formatDate(date)
 
     return {
+      type,  
+      accountIncome,
+      accountExpense,
       description,
-      account,
       amount,
       date
     }
-  },
-  
-  validadeField() {
-    const {description, account, amount, date} = this.getValues()
-
-    if(description.trim() === "" ||
-        account.trim() === "" ||
-        amount.trim() === "" ||
-        date.trim() === ""){
-      throw new Error("Por favor, preencha todos os campos")
-    }
-  },
+  },  
 
   clearFields() {
-    this.description.value = "";
-    this.account.value = "";
-    this.amount.value = "";
-    this.date.value = "";
+    const {type, accountIncome, accountExpense, description, amount, date} = this.getElements();
+    type.value = "";
+    accountIncome.value = "";
+    accountExpense.value = "";
+    description.value = "";    
+    amount.value = "";
+    date.value = "";
   },
 
   submit(event) {
-    event.preventDefault()
+    event.preventDefault()    
     try {
       this.validadeField()
       const transaction = this.formatValues() 
